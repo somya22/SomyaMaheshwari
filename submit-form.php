46b76a705033b4561e673e5f7bbf7da87be610bc
somya22 <43912550+somya22@users.noreply.github.com>
2023-09-19 18:28:00 -0500

    Add files via upload

---
+++ submit-form.php
@@ -0,0 +1,16 @@
+if (isset($_POST['submit'])){
+    $to = "somyamaheshwari22@gmail.com"; // recipient's email address
+    $subject = "New message from Contact Form";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $body = "From: $name\nEmail: $email\n\n$message";
+
+    // Send email
+    mail($to, $subject, $body);
+
+    // Redirect to thank-you page
+    header('Location: thank-you.html');
+    exit();
+}
